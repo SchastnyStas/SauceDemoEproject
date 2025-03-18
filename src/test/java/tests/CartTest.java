cb4da634dfa92f95700c2b74f336b46593740548
Stanislau Schastny <stasschastny@gmail.com>
2025-03-18 20:00:50 +0100

    Add SauceDemoEproject

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -1,18 +1,30 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 
 public class CartTest extends BaseTest {
 
-    @Test
-    public void checkProductPriceCartTest() {
+    @DataProvider(name = "products")
+    public Object[][] productsAndPrices() {
+        return new Object[][]{
+                {SAUCE_LABS_BACKPACK, "$29.99"},
+                {SAUCE_LABS_BOLT_T_SHIRT, "$15.99"},
+                {SAUCE_LABS_BIKE_LIGHT, "$9.99"},
+                {SAUCE_LABS_FLEECE_JACKET, "$49.99"},
+                {SAUCE_LABS_ONESIE, "$7.99"},
+                {TEST_ALL_THE_THINGS_T_SHIRT_RED, "$15.99"},
+        };
+    }
+
+    @Test(dataProvider = "products")
+    public void checkProductPriceCartTest(String productName, String price) {
         loginPage.openPage(LOGIN_PAGE_URL);
         loginPage.login(USERNAME, PASSWORD);
-        productsPage.addProductToCart(SAUCE_LABS_BOLT_T_SHIRT);
+        productsPage.addProductToCart(productName);
         cartPage.openPage(CART_PAGE_URL);
-        Assert.assertEquals(cartPage.getProductPrice(SAUCE_LABS_BOLT_T_SHIRT), "$15.99");
+        Assert.assertEquals(cartPage.getProductPrice(productName), price);
     }
 
     @Test
